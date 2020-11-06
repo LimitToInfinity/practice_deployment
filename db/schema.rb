1860e45684931ae445a912e4bb86413e070d86aa
LimitToInfinity <21stLimitless@gmail.com>
2020-11-06 13:36:06 -0700

    Changes database from sqlite3 to postgresql.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_06_201852) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.integer "age"
